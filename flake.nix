761dc6a1bccd4ab13c3030eed741ed5689c9462e
Taha <mt190502@mtaha.dev>
2025-01-25 18:57:12 +0300

    feat: update flakes

--- flake.nix
+++ flake.nix
@@ -18,7 +18,10 @@
       url = "github:alexghr/alacritty-theme.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix";
+    stylix = {
+      url = "github:danth/stylix/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
