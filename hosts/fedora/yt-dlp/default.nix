16b178bd90841738351eba8fa8aa12a6779dde92
Taha <mt190502@mtaha.dev>
2025-02-20 16:01:13 +0300

    feat: enhance yt-dlp configuration with metadata parsing and character renaming script

--- hosts/fedora/yt-dlp/default.nix
+++ hosts/fedora/yt-dlp/default.nix
@@ -5,18 +5,49 @@
     enable = true;
 
     settings = {
-      no-mtime = true;
-      parse-metadata = "\"playlist_index:%(track_number)s %(release_year|)s:%(meta_date)s track:(?i)(?P<track>.+)s+(?:([^)]*(?:master|edition|original|mix)[^)]*))s* album:(?i)(?P<album>.+)s+(?:([^)]*(?:master|edition)[^)]*))s*\"";
-      replace-in-metadata = "'artist' ',.+' ''";
+      convert-thumbnails = "jpg";
       embed-metadata = true;
       embed-thumbnail = true;
-      convert-thumbnails = "jpg";
+      exec = "$HOME/.config/yt-dlp/modify-and-trim-nonstandard-characters.sh";
+      no-mtime = true;
       ppa = "\"ffmpeg: -c:v mjpeg -vf crop=\\\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\\\"\"";
+      replace-in-metadata = "'artist' ',.+' ''";
     };
     extraConfig = ''
+      --parse-metadata "playlist_index:%(track_number)s"
+      --parse-metadata "%(release_year|)s:%(meta_date)s"
+      --parse-metadata "track:(?i)(?P<track>.+)s+(?:([^)]*(?:master|edition|original|mix)[^)]*))s*"
+      --parse-metadata "album:(?i)(?P<album>.+)s+(?:([^)]*(?:master|edition)[^)]*))s*"
       -f 251
       -x
       -N 8
     '';
+  };
+  xdg.configFile."yt-dlp/modify-and-trim-nonstandard-characters.sh" = {
+    text = ''
+      #!/usr/bin/env bash
+      OLDIFS=$IFS
+      IFS=$'\n'
+      YELLOW='\033[1;33m'
+      GREEN='\033[0;32m'
+      NC='\033[0m'
+      
+      for f in $(find $HOME/Music -depth -name '*[ğüşıöçĞÜŞİÖÇâîûêôÂÎÛÊÔ⧸？&]*'); do
+        new=$(echo "$f" | awk -F '/' '{print $NF}' | sed 's/&/feat./g') 
+        new=$(basename "$new" | sed 'y/ğüşıöçĞÜŞİÖÇâîûêôÂÎÛÊÔ⧸？/gusiocGUSIOCaiueoAIUEO|?/')
+        new_filename=$(echo "$f" | sed "s/$(basename "$f")/$new/")
+        if [[ -d "$new_filename" ]]; then 
+          cp -R "$f"/* "$new_filename"/
+          rm -rf "$f"
+          [[ -d "$new_filename" ]] && echo -e "''${YELLOW}Merged contents of \"$f\" to \"$new_filename\"''${NC}"
+        elif [[ "$new_filename" != "$f" ]] && [[ ! -f "$new_filename" ]]; then
+          mv "$f" "$new_filename"
+          [[ -f "$new_filename" ]] && echo -e "''${YELLOW}Renamed \"$f\" to \"$new_filename\"''${NC}"
+        fi
+      done
+      IFS=$OLDIFS
+      echo -e "''${GREEN}Renaming process finished.''${NC}"
+    '';
+    executable = true;
   };
 }
