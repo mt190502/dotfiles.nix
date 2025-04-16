7e234c7955ef8ae0d4ac9da949b435e9a5b23099
Taha <mt190502@mtaha.dev>
2025-04-16 14:31:58 +0300

    update(package): update zmem package

--- packages/zmem/default.nix
+++ packages/zmem/default.nix
@@ -10,10 +10,10 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "xeome";
     repo = pname;
     rev = version;
-    sha256 = "sha256-HkPDTXPkvE1u2VlFv26DaV9VRKjw225Pf1xUBHz0j1A=";
+    sha256 = "sha256-0BCt+Jz/571dIyju89b4H9n0byCnEEt3pdUzJ3EUVOM=";
   };
   useFetchCargoVendor = true;
-  cargoHash = "sha256-ogADsu7q+vRHUmo3LER0P5g36AsAlV1fa4jkPHe9Wxk=";
+  cargoHash = "sha256-cjDOGANGG9gfdW5OdEveGbhhYxxL96l1YGCVKMyVa1A=";
   meta = {
     description = "Advanced linux memory monitoring";
     homepage = "https://github.com/xeome/zmem";
