907081b8e8fa9e336fe734440518c84a9f874fbe
Taha <mt190502@mtaha.dev>
2025-04-26 18:31:03 +0300

    refactor(assets): update wallpapers

--- modules/home-manager/stylix/default.nix
+++ modules/home-manager/stylix/default.nix
@@ -9,6 +9,7 @@
 
   config.stylix = {
     enable = true;
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     # autoEnable = false;
     cursor = {
       package = pkgs.adwaita-icon-theme;
