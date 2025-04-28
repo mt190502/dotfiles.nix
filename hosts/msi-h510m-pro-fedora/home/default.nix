863cc7b8b755dcc7e276f0396bf6f6a65a87f8dc
Taha <mt190502@mtaha.dev>
2025-04-28 19:26:30 +0300

    fix(tmpfiles): add missing Discord IPC configuration

--- hosts/msi-h510m-pro-fedora/home/default.nix
+++ hosts/msi-h510m-pro-fedora/home/default.nix
@@ -55,6 +55,7 @@
   xdg.configFile."user-tmpfiles.d/home-manager.conf" = {
     text = ''
       L %t/discord-ipc-0 - - - - .flatpak/dev.vencord.Vesktop/xdg-run/discord-ipc-0
+      L %t/app/com.discordapp.Discord/discord-ipc-0 - - - - %t/.flatpak/dev.vencord.Vesktop/xdg-run/discord-ipc-0
     '';
     onChange = "${pkgs.systemd}/bin/systemd-tmpfiles --user --create";
   };
