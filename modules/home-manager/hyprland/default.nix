96850ba607fb0cc49d031bb2730775809de61247
Taha <mt190502@mtaha.dev>
2025-05-09 12:58:52 +0300

    refactor(scripts): replace pkgs.substituteAll with pkgs.substitute for deprecated substituteAll

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -31,32 +31,44 @@ in
         lib.map (path: {
           name = "hypr/scripts.d/${path}";
           value = {
-            source = pkgs.substituteAll {
-              src = ./scripts.d + "/${path}";
-              env = {
-                alacritty = lib.getExe config.wrapped.alacritty;
-                bash = lib.getExe pkgs.bash;
-                cliphist = lib.getExe pkgs.cliphist;
-                grim = lib.getExe pkgs.grim;
-                hyprland = config.wrapped.hyprland;
-                imagemagick = config.wrapped.imagemagick;
-                imv = config.wrapped.imv;
-                jq = lib.getExe pkgs.jq;
-                mako = pkgs.mako;
-                ncmpcpp = lib.getExe pkgs.ncmpcpp;
-                notify-send = lib.getExe pkgs.libnotify;
-                slurp = lib.getExe pkgs.slurp;
-                swappy = lib.getExe pkgs.swappy;
-                swaync = lib.getExe pkgs.swaynotificationcenter;
-                tesseract = lib.getExe pkgs.tesseract;
-                tmux = lib.getExe pkgs.tmux;
-                trans = lib.getExe pkgs.translate-shell;
-                wl_clipboard = pkgs.wl-clipboard;
-                wofi = lib.getExe pkgs.wofi;
-                wtype = lib.getExe pkgs.wtype;
-              };
-            };
             executable = true;
+            source =
+              let
+                substitutions = lib.flatten (
+                  lib.mapAttrsToList
+                    (k: v: [
+                      "--replace"
+                      "@${k}@"
+                      "${v}"
+                    ])
+                    {
+                      alacritty = lib.getExe config.wrapped.alacritty;
+                      bash = lib.getExe pkgs.bash;
+                      cliphist = lib.getExe pkgs.cliphist;
+                      grim = lib.getExe pkgs.grim;
+                      hyprland = config.wrapped.hyprland;
+                      imagemagick = config.wrapped.imagemagick;
+                      imv = config.wrapped.imv;
+                      jq = lib.getExe pkgs.jq;
+                      mako = pkgs.mako;
+                      ncmpcpp = lib.getExe pkgs.ncmpcpp;
+                      notify-send = lib.getExe pkgs.libnotify;
+                      slurp = lib.getExe pkgs.slurp;
+                      swappy = lib.getExe pkgs.swappy;
+                      swaync = lib.getExe pkgs.swaynotificationcenter;
+                      tesseract = lib.getExe pkgs.tesseract;
+                      tmux = lib.getExe pkgs.tmux;
+                      trans = lib.getExe pkgs.translate-shell;
+                      wl_clipboard = pkgs.wl-clipboard;
+                      wofi = lib.getExe pkgs.wofi;
+                      wtype = lib.getExe pkgs.wtype;
+                    }
+                );
+              in
+              pkgs.substitute {
+                src = ./scripts.d + "/${path}";
+                inherit substitutions;
+              };
           };
         }) (builtins.attrNames (builtins.readDir ./scripts.d))
       )
--- modules/home-manager/sway/default.nix
+++ modules/home-manager/sway/default.nix
@@ -31,34 +31,46 @@ in
         lib.map (path: {
           name = "sway/scripts.d/${path}";
           value = {
-            source = pkgs.substituteAll {
-              src = ./scripts.d + "/${path}";
-              env = {
-                alacritty = lib.getExe config.wrapped.alacritty;
-                bash = lib.getExe pkgs.bash;
-                cliphist = lib.getExe pkgs.cliphist;
-                grim = lib.getExe pkgs.grim;
-                imagemagick = config.wrapped.imagemagick;
-                imv = config.wrapped.imv;
-                jq = lib.getExe pkgs.jq;
-                mako = pkgs.mako;
-                ncmpcpp = lib.getExe pkgs.ncmpcpp;
-                notify-send = lib.getExe pkgs.libnotify;
-                slurp = lib.getExe pkgs.slurp;
-                # swaylock = lib.getExe pkgs.swaylock;     # not available in non nixos systems
-                swappy = lib.getExe pkgs.swappy;
-                sway = config.wrapped.sway;
-                swaync = lib.getExe pkgs.swaynotificationcenter;
-                tesseract = lib.getExe pkgs.tesseract;
-                tmux = lib.getExe pkgs.tmux;
-                trans = lib.getExe pkgs.translate-shell;
-                wl_clipboard = pkgs.wl-clipboard;
-                wofi = lib.getExe pkgs.wofi;
-                wtype = lib.getExe pkgs.wtype;
-              };
-            };
             executable = true;
-          };
+            source =
+              let
+                substitutions = lib.flatten (
+                  lib.mapAttrsToList
+                    (k: v: [
+                      "--replace"
+                      "@${k}@"
+                      "${v}"
+                    ])
+                    {
+                      alacritty = lib.getExe config.wrapped.alacritty;
+                      bash = lib.getExe pkgs.bash;
+                      cliphist = lib.getExe pkgs.cliphist;
+                      grim = lib.getExe pkgs.grim;
+                      imagemagick = config.wrapped.imagemagick;
+                      imv = config.wrapped.imv;
+                      jq = lib.getExe pkgs.jq;
+                      mako = pkgs.mako;
+                      ncmpcpp = lib.getExe pkgs.ncmpcpp;
+                      notify-send = lib.getExe pkgs.libnotify;
+                      slurp = lib.getExe pkgs.slurp;
+                      # swaylock = lib.getExe pkgs.swaylock;     # not available in non nixos systems
+                      swappy = lib.getExe pkgs.swappy;
+                      sway = config.wrapped.sway;
+                      swaync = lib.getExe pkgs.swaynotificationcenter;
+                      tesseract = lib.getExe pkgs.tesseract;
+                      tmux = lib.getExe pkgs.tmux;
+                      trans = lib.getExe pkgs.translate-shell;
+                      wl_clipboard = pkgs.wl-clipboard;
+                      wofi = lib.getExe pkgs.wofi;
+                      wtype = lib.getExe pkgs.wtype;
+                    }
+                );
+              in
+              pkgs.substitute {
+                src = ./scripts.d + "/${path}";
+                inherit substitutions;
+              };
+           };
         }) (builtins.attrNames (builtins.readDir ./scripts.d))
       )
     );
