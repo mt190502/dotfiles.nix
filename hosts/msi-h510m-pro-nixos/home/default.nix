0453118be506a934b4b6053fd394553fe7a318d3
Taha <mt190502@mtaha.dev>
2025-05-21 22:03:38 +0300

    refactor(users): added automatically user import

--- hosts/msi-h510m-pro-nixos/home/default.nix
+++ hosts/msi-h510m-pro-nixos/home/default.nix
@@ -1,1 +1,12 @@
-{ home-manager.users.taha = import ./taha; }
+let
+  userDirs = builtins.attrNames (builtins.readDir ./.);
+  validUsers = builtins.filter (name: builtins.pathExists (./. + "/${name}/default.nix")) userDirs;
+in
+{
+  home-manager.users = builtins.listToAttrs (
+    builtins.map (name: {
+      inherit name;
+      value = import ./${name};
+    }) validUsers
+  );
+}
