80fccaf8d278f96e128c1c169ad91ea0cff165d1
Taha <mt190502@mtaha.dev>
2025-05-22 14:59:00 +0300

    feat(nixos): allow ports for syncthing into home-manager setup

--- hosts/msi-h510m-pro-nixos/host/networking/default.nix
+++ hosts/msi-h510m-pro-nixos/host/networking/default.nix
@@ -3,5 +3,15 @@
 {
   networking = {
     hostName = "190502";
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22000
+      ];
+      allowedUDPPorts = [
+        21027
+        22000
+      ];
+    };
   };
 }
