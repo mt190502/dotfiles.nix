3672f957d883c420ec9dca42df573cf1e95ef44c
Taha <mt190502@mtaha.dev>
2025-05-26 00:03:21 +0300

    refactor(mpd): streamline service configuration and remove unused inputs

--- modules/home-manager/mpd/default.nix
+++ modules/home-manager/mpd/default.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   config,
   lib,
   pkgs,
@@ -33,86 +32,75 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.mpd = {
-      enable = cfg.bundle == "mpd";
-      dataDir = "${home}/.cache/mpd";
-      musicDirectory = "${home}/Music";
-      playlistDirectory = "${home}/Music/Playlists";
-      network = {
-        listenAddress = "any";
-        port = 6600;
-        startWhenNeeded = true;
-      };
-      extraConfig = ''
-        audio_output {
-          type        "pipewire"
-          name        "PipeWire Audio Server"
+    services =
+      if (cfg.bundle == "mpd") then
+        {
+          mpd = {
+            enable = true;
+            dataDir = "${home}/.cache/mpd";
+            musicDirectory = "${home}/Music";
+            playlistDirectory = "${home}/Music/Playlists";
+            network = {
+              listenAddress = "any";
+              port = 6600;
+              startWhenNeeded = true;
+            };
+            extraConfig = ''
+              audio_output {
+                type        "pipewire"
+                name        "PipeWire Audio Server"
+              }
+            '';
+          };
+          mpd-discord-rpc = {
+            enable = cfg.discordrpc && cfg.enable;
+            settings = {
+              hosts = [ "localhost:6600" ];
+              format = {
+                details = "$title ($date)";
+                state = "$artist / $album";
+                timestamp = "elapsed";
+              };
+            };
+          };
+          mpdris2-rs.enable = true;
         }
-      '';
-    };
-
-    home.packages = lib.mkIf (cfg.bundle == "mpd") [
-      inputs.self.packages."${pkgs.system}".mpdris2-rs
-    ];
-    systemd.user.services.mpdris2-rs = lib.mkIf (cfg.bundle == "mpd") {
-      Unit = {
-        Description = "Music Player Daemon D-Bus Bridge";
-        Wants = "mpd.service";
-        After = "mpd.service";
-      };
-
-      Service = {
-        Restart = "on-failure";
-        ExecStart = "${inputs.self.packages.${pkgs.system}.mpdris2-rs}/bin/mpdris2-rs";
-        BusName = "org.mpris.MediaPlayer2.mpd";
-      };
-
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-    };
-
-    services.mopidy = {
-      enable = cfg.bundle == "mopidy";
-      extensionPackages = with pkgs; [
-        mopidy-jellyfin
-        mopidy-mpd
-        mopidy-mpris
-      ];
-      settings = {
-        file = {
-          enabled = true;
-          media_dirs = [
-            "$XDG_MUSIC_DIR|Music"
-          ];
-          follow_symlinks = true;
-          excluded_file_extensions = [
-            ".html"
-            ".zip"
-            ".jpg"
-            ".jpeg"
-            ".png"
-          ];
+      else if (cfg.bundle == "mopidy") then
+        {
+          mopidy = {
+            enable = cfg.bundle == "mopidy";
+            extensionPackages = with pkgs; [
+              mopidy-jellyfin
+              mopidy-mpd
+              mopidy-mpris
+            ];
+            settings = {
+              file = {
+                enabled = true;
+                media_dirs = [
+                  "$XDG_MUSIC_DIR|Music"
+                ];
+                follow_symlinks = true;
+                excluded_file_extensions = [
+                  ".html"
+                  ".zip"
+                  ".jpg"
+                  ".jpeg"
+                  ".png"
+                ];
+              };
+              mpd = {
+                hostname = "::";
+              };
+            };
+            #extraConfigFiles = [
+            #  "${home}/.config/mopidy/jellyfin.conf"
+            #];
+          };
+        }
+      else
+        {
+          mpd.enable = false;
         };
-        mpd = {
-          hostname = "::";
-        };
-      };
-      #extraConfigFiles = [
-      #  "${home}/.config/mopidy/jellyfin.conf"
-      #];
-    };
-
-    services.mpd-discord-rpc = {
-      enable = cfg.discordrpc && cfg.enable;
-      settings = {
-        hosts = [ "localhost:6600" ];
-        format = {
-          details = "$title ($date)";
-          state = "$artist / $album";
-          timestamp = "elapsed";
-        };
-      };
-    };
   };
 }
