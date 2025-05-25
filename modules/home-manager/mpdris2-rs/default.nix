b5bf9d18c3d498f9a5333c97379249cc56904daf
Taha <mt190502@mtaha.dev>
2025-05-26 00:02:51 +0300

    refactor(packages): move mpdris2-rs and pomobar packages to home-manager module

---
+++ modules/home-manager/mpdris2-rs/default.nix
@@ -0,0 +1,63 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib) concatStringsSep mkEnableOption;
+  inherit (pkgs) rustPlatform fetchFromGitHub;
+
+  cfg = config.services.mpdris2-rs;
+  package = rustPlatform.buildRustPackage rec {
+    pname = "mpdris2-rs";
+    version = "1.0.0-beta.1";
+    src = fetchFromGitHub {
+      owner = "szclsya";
+      repo = pname;
+      rev = version;
+      sha256 = "sha256-c9CI5KaC9wyfnYUvAIdq/4fznb7ehe5qbUiQ9ooPG+M=";
+    };
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-Lbs94OSyLd4hGMUeDGbjaLstd7ACfE7Tbrbz3uAyKoY=";
+    meta = {
+      description = "MPRIS2 client for MPD written in Rust";
+      homepage = "https://github.com/szclsya/mpdris2-rs";
+      mainProgram = "mpdris2-rs";
+    };
+  };
+in
+{
+  options.services.mpdris2-rs = {
+    enable = mkEnableOption "Enable mpdris2-rs service";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = package;
+      description = "The mpdris2-rs package to use.";
+    };
+    extraArgs = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Extra arguments to pass to the mpdris2-rs executable.";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    systemd.user.services.mpdris2-rs = {
+      Unit = {
+        Description = "Music Player Daemon D-Bus Bridge";
+        Wants = "mpd.service";
+        After = "mpd.service";
+      };
+      Service = {
+        Type = "dbus";
+        Restart = "on-failure";
+        ExecStart = "${lib.getExe package} ${concatStringsSep " " cfg.extraArgs}";
+        BusName = "org.mpris.MediaPlayer2.mpd";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
+}
