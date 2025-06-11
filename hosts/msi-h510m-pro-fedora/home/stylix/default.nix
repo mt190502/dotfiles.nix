3e129c9fcdecef101430ace18bf2996906b00778
Taha <mt190502@mtaha.dev>
2025-06-11 13:21:16 +0300

    update(host): change fedora host wallpaper

--- hosts/msi-h510m-pro-fedora/home/stylix/default.nix
+++ hosts/msi-h510m-pro-fedora/home/stylix/default.nix
@@ -3,6 +3,6 @@
 {
   config.stylix = {
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/apathy.yaml";
-    image = ../../../../assets/wallpaper8.jpg;
+    image = ../../../../assets/wallpaper12.png;
   };
 }
