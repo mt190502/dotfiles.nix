1b6f0ddfe5d9be6618ca31ce1bb8bec3c10f25b6
Taha <mt190502@mtaha.dev>
2025-06-16 13:00:44 +0300

    feat(lanzaboote): add lanzaboote module for support secure boot

--- flake.nix
+++ flake.nix
@@ -26,6 +26,10 @@
       url = "github:alexghr/alacritty-theme.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak/?ref=latest";
     };
