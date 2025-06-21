460e2559a2878bfd32d5ec311064d9cf2c1616d8
Taha <mt190502@mtaha.dev>
2025-06-21 18:34:27 +0300

    update(module): update mpdris2-rs version to 1.0.0

--- modules/home-manager/mpdris2-rs/default.nix
+++ modules/home-manager/mpdris2-rs/default.nix
@@ -12,15 +12,15 @@ let
   cfg = config.services.mpdris2-rs;
   package = rustPlatform.buildRustPackage rec {
     pname = "mpdris2-rs";
-    version = "1.0.0-beta.1";
+    version = "1.0.0";
     src = fetchFromGitHub {
       owner = "szclsya";
       repo = pname;
       rev = version;
-      sha256 = "sha256-c9CI5KaC9wyfnYUvAIdq/4fznb7ehe5qbUiQ9ooPG+M=";
+      sha256 = "sha256-Bx/mc2wFki0GX9ej8ZIfeXybX5zek/bA5+D0k7XlNnQ=";
     };
     useFetchCargoVendor = true;
-    cargoHash = "sha256-Lbs94OSyLd4hGMUeDGbjaLstd7ACfE7Tbrbz3uAyKoY=";
+    cargoHash = "sha256-AhzSA26KgFCL03W0+TOTSy0e5t4owA3TUM/+mbI6dmg=";
     meta = {
       description = "MPRIS2 client for MPD written in Rust";
       homepage = "https://github.com/szclsya/mpdris2-rs";
