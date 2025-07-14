0a5aa49221e4d50e022f02d947ddbb2aee4dcf98
Taha <mt190502@mtaha.dev>
2025-07-14 17:41:31 +0300

    feat(nixos): enable fprintd service

--- hosts/lenovo-thinkpad-e14-nixos/host/system/default.nix
+++ hosts/lenovo-thinkpad-e14-nixos/host/system/default.nix
@@ -18,6 +18,21 @@
     xdg-user-dirs-gtk
     xdg-utils
   ];
+  services = {
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-elan;
+      };
+    };
+  };
+  systemd.services = {
+    fprintd = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.Type = "simple";
+    };
+  };
   xdg = {
     autostart.enable = true;
     portal = {
