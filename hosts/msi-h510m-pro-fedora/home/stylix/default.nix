c5e5db2273e02e7fd31e7dd865aac487aac18443
Taha <mt190502@mtaha.dev>
2025-08-09 23:44:15 +0300

    update(stylix): change wallpaper image at msi-h510m-pro-fedora

--- hosts/msi-h510m-pro-fedora/home/stylix/default.nix
+++ hosts/msi-h510m-pro-fedora/home/stylix/default.nix
@@ -3,6 +3,6 @@
 {
   config.stylix = {
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/apathy.yaml";
-    image = ../../../../assets/wallpaper13.png;
+    image = ../../../../assets/wallpaper14.png;
   };
 }
