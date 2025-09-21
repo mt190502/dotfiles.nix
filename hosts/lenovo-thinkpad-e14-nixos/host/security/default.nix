9b42008533744c158e91b2805bbb8492c225a5c6
Taha <mt190502@mtaha.dev>
2025-09-21 18:46:26 +0300

    feat(security): disable fprint authentication at tty

--- hosts/lenovo-thinkpad-e14-nixos/host/security/default.nix
+++ hosts/lenovo-thinkpad-e14-nixos/host/security/default.nix
@@ -3,6 +3,7 @@
     polkit.enable = true;
     pam.services = {
       "gtklock".enable = true;
+      "login".fprintAuth = false;
       "mate-polkit".enable = true;
       "swaylock".enable = true;
       "passwd".enableGnomeKeyring = true;
