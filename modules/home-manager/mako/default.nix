a52272eceb0d70d4c7d45d800f5698c5a6788939
Taha <mt190502@mtaha.dev>
2025-10-02 17:43:57 +0300

    refactor(module): align service config with settings attribute for mako

--- modules/home-manager/mako/default.nix
+++ modules/home-manager/mako/default.nix
@@ -8,27 +8,29 @@ in
   config = lib.mkIf (cfg.preferred.notifier == "mako") {
     services.mako = with config.stylix.customColors.withHashtag; {
       enable = true;
-      backgroundColor = background + makoOpacity;
-      borderColor = active;
-      textColor = text;
-      progressColor = "over ${active}";
-      borderRadius = 0;
-      borderSize = 5;
-      defaultTimeout = 10000;
-      font = "${config.stylix.fonts.sansSerif.name} ${builtins.toString config.stylix.fonts.sizes.applications}";
-      ignoreTimeout = true;
-      layer = "overlay";
-      margin = "16";
-      maxIconSize = 64;
-      sort = "-time";
-      extraConfig = ''
-        [urgency=high]
-        border-color=${urgent}
-        default-timeout=0
+      settings = {
+        backgroundColor = background + makoOpacity;
+        borderColor = active;
+        textColor = text;
+        progressColor = "over ${active}";
+        borderRadius = 0;
+        borderSize = 5;
+        defaultTimeout = 10000;
+        font = "${config.stylix.fonts.sansSerif.name} ${builtins.toString config.stylix.fonts.sizes.applications}";
+        ignoreTimeout = true;
+        layer = "overlay";
+        margin = "16";
+        maxIconSize = 64;
+        sort = "-time";
 
-        [mode=dnd]
-        invisible=1
-      '';
+        "mode-dnd" = {
+          invisible = true;
+        };
+        "urgency=high" = {
+          border-color = urgent;
+          default-timeout = 0;
+        };
+      };
     };
   };
 }
