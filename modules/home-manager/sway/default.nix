fbff4a69fb78cb74da957ea670a6152c0e3b4a46
Taha <mt190502@mtaha.dev>
2025-10-21 12:34:57 +0300

    refactor(module): fix executable paths for imv-wayland and swaynag for sway configs

--- modules/home-manager/sway/default.nix
+++ modules/home-manager/sway/default.nix
@@ -48,13 +48,14 @@ in
                       bash = lib.getExe pkgs.bash;
                       grim = lib.getExe pkgs.grim;
                       imagemagick = config.wrapped.imagemagick;
-                      imv-wayland = lib.getExe' pkgs.imv "imv-wayland";
+                      imv-wayland = lib.getExe' config.wrapped.imv "imv-wayland";
                       jq = lib.getExe pkgs.jq;
                       ncmpcpp = lib.getExe pkgs.ncmpcpp;
                       notify-send = lib.getExe pkgs.libnotify;
                       slurp = lib.getExe pkgs.slurp;
                       swappy = lib.getExe pkgs.swappy;
                       swaymsg = lib.getExe' config.wrapped.sway "swaymsg";
+                      swaynag = lib.getExe' config.wrapped.sway "swaynag";
                       tesseract = lib.getExe pkgs.tesseract;
                       tmux = lib.getExe pkgs.tmux;
                       wl_clipboard = pkgs.wl-clipboard;
