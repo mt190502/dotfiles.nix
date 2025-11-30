48295d5333fce9b7f526c62407a1fbf4178f4e58
Taha <mt190502@mtaha.dev>
2025-11-30 16:50:26 +0300

    update(assets): add new wallpaper and update wallpaper on msi-h510m-pro-fedora

--- hosts/msi-h510m-pro-fedora/home/stylix/default.nix
+++ hosts/msi-h510m-pro-fedora/home/stylix/default.nix
@@ -3,6 +3,6 @@
 {
   config.stylix = {
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/apathy.yaml";
-    image = ../../../../assets/wallpaper9.jpg;
+    image = ../../../../assets/wallpaper15.png;
   };
 }
