7e7abda9b4671c2b7bf48b370d45bb33e01c4359
Taha <mt190502@mtaha.dev>
2025-12-03 20:04:39 +0300

    feat(module): add support for building Raycast extensions in Vicinae
    Implement genRaycastExtensions function to fetch and build Raycast
    extensions from the official repository. The function handles npm
    package building with sparse checkout and includes the Raycast CLI
    binary. Add 1password, chatgpt, tailscale, and word-count extensions.

--- modules/home-manager/vicinae/default.nix
+++ modules/home-manager/vicinae/default.nix
@@ -8,9 +8,50 @@
 
 let
   cfg = config.moduleopts.home-manager;
+  
+  rayCli = pkgs.fetchurl {
+    url = "https://cli.raycast.com/1.86.0-alpha.65/linux/ray";
+    sha256 = "sha256-UgDA2hIH7HwKl3j4UEGIlvh6eE+IWUlSML0wloHFPQw=";
+  };
+
   getVicinaeExtensions =
     names:
     map (name: inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}.${name}) names;
+  genRaycastExtensions =
+    with pkgs;
+    names:
+    let
+      raycastRepo = fetchFromGitHub {
+        owner = "raycast";
+        repo = "extensions";
+        rev = "bc92e53ae972e41a44800b2a4763a5b7bf69122e";
+        sha256 = "sha256-h5syKKafr0YUIZn4ky89yQx061svX7cL8R5ekxZMyUA=";
+        sparseCheckout = map (name: "/extensions/${name}") names;
+      };
+    in
+    map (
+      name:
+      buildNpmPackage rec {
+        inherit name;
+        src = raycastRepo + "/extensions/${name}";
+        buildPhase = ''
+          runHook preBuild
+          mkdir -p node_modules/@raycast/api/bin/linux
+          cp ${rayCli} node_modules/@raycast/api/bin/linux/ray
+          chmod +x node_modules/@raycast/api/bin/linux/ray
+          npm run build
+          runHook postBuild
+        '';
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/
+          cp -r /build/.config/*/extensions/${name}/* $out/
+          runHook postInstall
+        '';
+        npmConfigHook = importNpmLock.npmConfigHook;
+        npmDeps = importNpmLock { npmRoot = src; };
+      }
+    ) names;
 in
 {
   config = lib.mkIf (cfg.preferred.menu == "vicinae") {
@@ -26,6 +67,12 @@ in
           "nix"
           "ssh"
           "stocks"
+        ])
+        ++ (genRaycastExtensions [
+          "1password"
+          "chatgpt"
+          "tailscale"
+          "word-count"
         ]);
       themes = {
         stylix = with config.stylix; {
