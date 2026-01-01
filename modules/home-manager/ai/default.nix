79aa95144ef25659eb78dd88092e9700dd3116fb
Taha <mt190502@mtaha.dev>
2026-01-01 22:03:09 +0300

    feat(module): remove gemini-cli configuration from AI module

--- modules/home-manager/ai/default.nix
+++ modules/home-manager/ai/default.nix
@@ -16,43 +16,25 @@ in
       description = "ai-tools";
     };
   };
-  config = lib.mkIf cfg.enable (
-    with pkgs-unstable;
-    {
-      home.packages = [
-        cursor-cli
-      ];
-      programs.gemini-cli = {
-        enable = true;
-        package = gemini-cli;
-        defaultModel = "gemini-2.5-pro";
-        settings = {
-          general = {
-            vimMode = true;
-          };
-          security = {
-            auth = {
-              selectedType = "oauth-personal";
-            };
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs-unstable; [
+      cursor-cli
+    ];
+    programs.opencode = {
+      enable = true;
+      package = pkgs-unstable.opencode;
+      settings = {
+        # theme = "flexoki";
+        mcp = {
+          context7 = {
+            type = "remote";
+            url = "https://mcp.context7.com/mcp";
           };
         };
-      };
-      programs.opencode = {
-        enable = true;
-        package = opencode;
-        settings = {
-          # theme = "flexoki";
-          mcp = {
-            context7 = {
-              type = "remote";
-              url = "https://mcp.context7.com/mcp";
-            };
-          };
-          permission = {
-            bash = "ask";
-          };
+        permission = {
+          bash = "ask";
         };
       };
-    }
-  );
+    };
+  };
 }
