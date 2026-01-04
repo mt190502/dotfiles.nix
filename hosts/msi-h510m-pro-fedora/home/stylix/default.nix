cc8c0e118df24cf4423f3116fbd8882272f91e54
Taha <mt190502@mtaha.dev>
2026-01-04 23:23:47 +0300

    refactor(assets): removed some wallpapers and added new wallpapers

--- hosts/msi-h510m-pro-fedora/home/stylix/default.nix
+++ hosts/msi-h510m-pro-fedora/home/stylix/default.nix
@@ -3,6 +3,6 @@
 {
   config.stylix = {
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/apathy.yaml";
-    image = ../../../../assets/wallpaper15.png;
+    image = ../../../../assets/wallpaper10.png;
   };
 }
