9f7aea2c5345d430ae54d1d6ff21add30592e406
Taha <mt190502@mtaha.dev>
2026-05-12 13:08:56 +0300

    refactor(host): change signing key for taha user on macbook-m3-air-darwin-work host

--- hosts/macbook-m3-air-darwin-work/home/taha/git/default.nix
+++ hosts/macbook-m3-air-darwin-work/home/taha/git/default.nix
@@ -1,5 +1,8 @@
 { lib, ... }:
 
 {
-  programs.git.settings.gpg."ssh".program = lib.mkForce "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+  programs.git.settings = {
+    signing.key = lib.mkForce "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmkUvMmx4scwZkRgOQqcqMorm8zLuqXK3HUYbsunrpl";
+    gpg."ssh".program = lib.mkForce "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+  };
 }
