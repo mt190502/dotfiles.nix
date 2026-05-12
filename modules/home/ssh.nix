b3bcd3b4ae1bccc29493e5cbe1db4c8d701701c9
Taha <mt190502@mtaha.dev>
2026-05-12 20:43:20 +0300

    chore(lint): fix lint issues

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -5,42 +5,43 @@
 }:
 
 let
-  hostDirNames = lib.sort lib.lessThan (
-    builtins.attrNames (
-      lib.filterAttrs (_: t: t == "directory") (builtins.readDir (inputs.self + "/hosts"))
-    )
-  );
-
   hosts = lib.filter (h: h != null) (
-    map (
-      name:
-      let
-        cfgPath = inputs.self + "/hosts/${name}/config.nix";
-        netPath = inputs.self + "/hosts/${name}/host/networking/default.nix";
-      in
-      if !(builtins.pathExists cfgPath && builtins.pathExists netPath) then
-        null
-      else
+    map
+      (
+        name:
         let
-          hostCfg = import cfgPath;
-          netMod = import netPath;
-          netCfg = if builtins.isFunction netMod then netMod { inherit lib; } else netMod;
-          hostName = netCfg.networking.hostName or null;
-          user =
-            hostCfg.primaryUser or (hostCfg.user
-              or (if hostCfg ? users && hostCfg.users != [ ] then builtins.head hostCfg.users else null)
-            );
+          cfgPath = inputs.self + "/hosts/${name}/config.nix";
+          netPath = inputs.self + "/hosts/${name}/host/networking/default.nix";
         in
-        if hostName == null || user == null then null else { inherit name hostName user; }
-    ) hostDirNames
+        if !(builtins.pathExists cfgPath && builtins.pathExists netPath) then
+          null
+        else
+          let
+            hostCfg = import cfgPath;
+            netMod = import netPath;
+            netCfg = if builtins.isFunction netMod then netMod { inherit lib; } else netMod;
+            hostName = netCfg.networking.hostName or null;
+            user =
+              hostCfg.primaryUser or (hostCfg.user
+                or (if hostCfg ? users && hostCfg.users != [ ] then builtins.head hostCfg.users else null)
+              );
+          in
+          if hostName == null || user == null then null else { inherit name hostName user; }
+      )
+      (
+        lib.sort lib.lessThan (
+          builtins.attrNames (
+            lib.filterAttrs (_: t: t == "directory") (builtins.readDir (inputs.self + "/hosts"))
+          )
+        )
+      )
   );
-
   hostMatchBlocks = lib.listToAttrs (
     map (h: {
-      name = h.name;
+      inherit (h) name;
       value = {
+        inherit (h) user;
         hostname = h.hostName;
-        user = h.user;
         forwardAgent = true;
       };
     }) hosts
