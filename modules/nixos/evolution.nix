985d04c9831fcc4e97094137539ec3f5e7f22c2b
Taha <mt190502@mtaha.dev>
2026-05-17 23:58:49 +0300

    feat(module): add goa support

--- modules/nixos/evolution.nix
+++ modules/nixos/evolution.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    gnome-online-accounts-gtk
+  ];
   programs.evolution = {
     enable = true;
     plugins = with pkgs; [ ];
   };
-  services.gnome.evolution-data-server = {
-    enable = true;
-    plugins = with pkgs; [ ];
+  services.gnome = {
+    evolution-data-server = {
+      enable = true;
+      plugins = with pkgs; [ ];
+    };
+    gnome-online-accounts.enable = true;
   };
 }
