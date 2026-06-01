5c76257dc3ae9613373aa6a6481206f6f8db8627
Taha <mt190502@mtaha.dev>
2026-06-01 15:44:46 +0300

    fix(host): fix git options for macbook-m3-air-darwin-work

--- hosts/macbook-m3-air-darwin-work/home/taha/git/default.nix
+++ hosts/macbook-m3-air-darwin-work/home/taha/git/default.nix
@@ -1,9 +1,9 @@
 { lib, ... }:
 
 {
-  programs.git.settings = lib.mkForce rec {
-    user.signingKey = signing.key;
-    signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmkUvMmx4scwZkRgOQqcqMorm8zLuqXK3HUYbsunrpl";
-    gpg."ssh".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+  programs.git.settings = rec {
+    user.signingKey = lib.mkForce signing.key;
+    signing.key = lib.mkForce "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOmkUvMmx4scwZkRgOQqcqMorm8zLuqXK3HUYbsunrpl";
+    gpg."ssh".program = lib.mkForce "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
   };
 }
