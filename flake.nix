908be0fe26165d9a2876060e36d68fd2ab998624
Taha <mt190502@mtaha.dev>
2026-06-03 23:24:53 +0300

    refactor(module): pin stylix dependency version and update gtk module for new stylix module

--- flake.nix
+++ flake.nix
@@ -10,10 +10,6 @@
       url = "github:NixOS/nixpkgs/nixos-26.05";
       flake = true;
     };
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -68,7 +64,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix";
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     vicinae-extensions = {
--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -15,17 +15,24 @@
       name = config.iconthemecfg.dark;
       package = lib.mkForce config.iconthemecfg.package;
     };
-    gtk4 = {
-      inherit (config.gtk) theme;
-      extraConfig = {
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintslight";
-        gtk-xft-rgba = "rgb";
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
+    gtk4 =
+      let
+        theme = config.gtk.theme;
+      in
+      {
+        theme = lib.mkIf (theme != null) {
+          inherit (theme) name;
+          package = lib.mkForce theme.package;
+        };
+        extraConfig = {
+          gtk-xft-antialias = 1;
+          gtk-xft-hinting = 1;
+          gtk-xft-hintstyle = "hintslight";
+          gtk-xft-rgba = "rgb";
+          gtk-enable-event-sounds = 0;
+          gtk-enable-input-feedback-sounds = 0;
+        };
       };
-    };
     gtk3.extraConfig = {
       gtk-xft-antialias = 1;
       gtk-xft-hinting = 1;
