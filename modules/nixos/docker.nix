4086080fb7650fe84ee79ba3e740a6a28358ab90
Taha <mt190502@mtaha.dev>
2026-08-10 19:25:59 +0300

    refactor(module): added missing DNS and IPv6 option for docker daemon

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -5,6 +5,14 @@
     rootless = {
       enable = true;
       setSocketVariable = true;
+      daemon.settings = {
+        dns = [
+          "1.1.1.1"
+          "9.9.9.9"
+          "8.8.8.8"
+        ];
+        ipv6 = true;
+      };
     };
   };
 }
