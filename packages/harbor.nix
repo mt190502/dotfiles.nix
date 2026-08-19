541adce667ef6d0e8f57c8b490fed44a90e0afdc
Taha <mt190502@mtaha.dev>
2026-08-19 15:59:13 +0300

    refactor(package): updated harbor package

--- packages/harbor.nix
+++ packages/harbor.nix
@@ -1,93 +1,41 @@
 {
   lib,
-  stdenv,
-  dpkg,
+  appimageTools,
+  dwarfs,
   fetchurl,
-  autoPatchelfHook,
-  wrapGAppsHook3,
-  # Runtime libraries for autoPatchelfHook
-  mpv,
-  systemd,
-  openssl,
-  gtk3,
-  gdk-pixbuf,
-  cairo,
-  glib,
-  webkitgtk_4_1,
-  libsoup_3,
-  libgcc,
   libayatana-appindicator,
-  gst_all_1,
+  runCommand,
   ...
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+let
   pname = "harbor";
   version = "0.9.117";
-
-  src = fetchurl {
-    url = "https://github.com/harborstremio-linux/harbor-linux-builds/releases/download/beta-v${finalAttrs.version}/Harbor_${finalAttrs.version}-2_amd64.deb";
-    hash = "sha256-cuDsBNrcKaxDQ5GqhQDPpeDz1IojPjaRDSpr4Kaz1+g=";
+  appimage = fetchurl {
+    url = "https://github.com/harborstremio-linux/harbor-linux-builds/releases/download/beta-v${version}/Harbor_${version}_amd64.AppImage";
+    hash = "sha256-lW8EybyIWTZ1pWtdnzV4DisoHfnjfsO7tjsFCjxJV/g=";
   };
+  contents =
+    runCommand "${pname}-${version}-extracted"
+      {
+        nativeBuildInputs = [ dwarfs ];
+      }
+      ''
+        mkdir $out
+        dwarfsextract -i ${appimage} -o $out
+      '';
+in
+appimageTools.wrapAppImage {
+  inherit pname version;
+  src = contents;
 
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    wrapGAppsHook3
-  ];
+  extraPkgs = _: [ libayatana-appindicator ];
 
-  buildInputs = [
-    mpv
-    systemd
-    openssl
-    gtk3
-    gdk-pixbuf
-    cairo
-    glib
-    webkitgtk_4_1
-    libsoup_3
-    libgcc
-    libayatana-appindicator
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-  ];
+  passthru.src = appimage;
 
-  #~ libayatana-appindicator is dlopened at runtime; autoPatchelfHook
-  #~ won't add it to RUNPATH because it's not in NEEDED.
-  preFixup = ''
-    gappsWrapperArgs+=(
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libayatana-appindicator ]}
-    )
-  '';
-
-  dontConfigure = true;
-  dontBuild = true;
-
-  unpackPhase = ''
-    runHook preUnpack
-    dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
-    runHook postUnpack
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    #~ main binary
-    install -Dm755 usr/bin/harbor $out/bin/harbor
-
-    #~ bundled resources (fonts, notices)
-    mkdir -p "$out/lib"
-    cp -r "usr/lib/Harbor Beta" "$out/lib/Harbor Beta"
-
-    #~ desktop file
-    install -Dm644 "usr/share/applications/Harbor Beta.desktop" $out/share/applications/harbor.desktop
-    substituteInPlace $out/share/applications/harbor.desktop --replace-fail "Name=Harbor Beta" "Name=Harbor"
-
-    #~ icons
-    cp -r usr/share/icons $out/share/
-    runHook postInstall
+  extraInstallCommands = ''
+    install -Dm444 ${contents}/Harbor.desktop $out/share/applications/harbor.desktop
+    install -Dm444 ${contents}/harbor.png $out/share/icons/hicolor/256x256/apps/harbor.png
   '';
 
   meta = {
@@ -96,6 +44,5 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.mit;
     mainProgram = "harbor";
     platforms = [ "x86_64-linux" ];
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
-})
+}
